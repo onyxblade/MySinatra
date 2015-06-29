508f6375edd5e6049c17ca91b2f5f9216734842a
Cichol <cichol@live.cn>
2015-06-29 20:02:39 +0800

    update

--- lib/generator.rb
+++ lib/generator.rb
@@ -6,3 +6,17 @@ def generate_project(name)
   Dir.mkdir 'views'
   Dir.mkdir 'models'
 end
+
+def camelize(str)
+  str.split('_').map(&:capitalize).join
+end
+
+def generate_migration(name)
+  version = Time.now.utc.strftime("%Y%m%d%H%M%S")
+  File.new("./db/migrations/#{version}_#{name}.rb", 'w').write <<-EOF
+class #{camelize(name)} < ActiveRecord::Migration
+  def change
+  end
+end
+  EOF
+end
